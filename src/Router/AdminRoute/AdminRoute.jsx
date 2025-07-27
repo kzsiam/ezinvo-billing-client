39e34b7b91a832acc1ed0a4ec883208082dffe24
Kazee Siam <kzsiam@outlook.com>
2025-07-27 07:52:16 +0600

    admin route

--- src/Router/AdminRoute/AdminRoute.jsx
+++ src/Router/AdminRoute/AdminRoute.jsx
@@ -1,28 +1,28 @@
-// import React, { useContext } from 'react';
-// import UseAdmin from '@/SharedPages/UseAdmin/UseAdmin';
-// import { AuthContext } from '@/Contexts/AuthProvider';
-// import { Navigate, useLocation } from 'react-router';
+import React, { useContext } from 'react';
+import UseAdmin from '@/SharedPages/UseAdmin/UseAdmin';
+import { AuthContext } from '@/Contexts/AuthProvider';
+import { Navigate, useLocation } from 'react-router';
 
-// const AdminRoute = ({children}) => {
-//     const location = useLocation()
-//     const {user,loader} = useContext(AuthContext)
+const AdminRoute = ({children}) => {
+    const location = useLocation()
+    const {user,loader} = useContext(AuthContext)
 
-//     const [isAdmin,isAdminLoading] = UseAdmin(user?.email)
+    const [isAdmin,isAdminLoading] = UseAdmin(user?.email)
 
-//     if(loader || isAdminLoading){
-//         return <div className='mx-auto'>
-//         <span className="loading loading-bars loading-xs"></span>
-//         <span className="loading loading-bars loading-sm"></span>
-//         <span className="loading loading-bars loading-md"></span>
-//         <span className="loading loading-bars loading-lg"></span>
-//     </div>
-//     }
-//     if(user && isAdmin){
-//         return children;
-//     }
-//     return (
-//         <Navigate to={'/auth/login'} state={{ from: location }} replace></Navigate>
-//     );
-// };
+    if(loader || isAdminLoading){
+        return <div className='mx-auto'>
+        <span className="loading loading-bars loading-xs"></span>
+        <span className="loading loading-bars loading-sm"></span>
+        <span className="loading loading-bars loading-md"></span>
+        <span className="loading loading-bars loading-lg"></span>
+    </div>
+    }
+    if(user && isAdmin){
+        return children;
+    }
+    return (
+        <Navigate to={'/auth/login'} state={{ from: location }} replace></Navigate>
+    );
+};
 
-// export default AdminRoute;
+export default AdminRoute;
